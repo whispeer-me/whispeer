c49de42bea85ca18448822f42d29dcbe5cba2d1f
Nesim Tunc <nesimtunc@gmail.com>
2023-12-30 22:28:31 +0900

    Update navigation

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,60 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="main-nav">
+      <router-link to="/" class="nav-link">Home</router-link>
+      <router-link to="/about" class="nav-link">About</router-link>
     </nav>
+    <hr class="divider" />
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 @font-face {
   font-family: "LCD-Solid";
   src: url("@/assets/fonts/LCD-Solid.ttf.woff") format("woff"),
-    url("@/assets//fonts/LCD-Solid.ttf.svg#LCD-Solid") format("svg"),
+    url("@/assets/fonts/LCD-Solid.ttf.svg#LCD-Solid") format("svg"),
     url("@/assets/fonts/LCD-Solid.ttf.eot"),
     url("@/assets/fonts/LCD-Solid.ttf.eot?#iefix") format("embedded-opentype");
   font-weight: normal;
   font-style: normal;
 }
 
+#app {
+  font-family: "LCD-Solid", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
 body {
   background-color: #000;
 }
 
-nav {
+.divider {
+  border: none;
+  height: 2px;
+  background-color: #c4872b;
+  margin: 0;
+}
+
+.main-nav {
   padding: 30px;
+  background-color: rgba(0, 0, 0, 0.7);
+  text-align: left;
 
-  a {
-    font-weight: bold;
+  .nav-link {
+    font-family: "LCD-Solid", sans-serif;
+    font-size: 24px;
     color: #c4872b;
-    opacity: 0.3;
+    text-decoration: none;
+    margin: 0 15px;
+    opacity: 0.7;
+    transition: opacity 0.3s ease;
 
+    &:hover,
     &.router-link-exact-active {
-      color: #c4872b;
       opacity: 1;
     }
   }
