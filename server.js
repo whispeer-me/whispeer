132806119d058458e1fac61fb0b1367cc3561382
Nesim Tunc <nesimtunc@gmail.com>
2023-12-31 15:15:14 +0900

    Lint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const path = require('path');
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
 
 const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 const port = process.env.PORT || 80;
 app.listen(port);
