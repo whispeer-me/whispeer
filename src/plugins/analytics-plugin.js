70ff07dc2c35b13a481cee572a41300eac93c68f
Nesim Tunc <nesimtunc@gmail.com>
2024-01-01 20:42:18 +0900

    Respect user's Do NOT Track preference in the browser
    Won't track any pageview nor event for the user based on the preference

--- src/plugins/analytics-plugin.js
+++ src/plugins/analytics-plugin.js
@@ -1,14 +1,24 @@
 import Plausible from "plausible-tracker";
 
+// This is a mock analytics plugin that does nothing
+// to respect user's privacy
+// Instead of checking on each page and event; having an empty mock is better
+const mockAnalytics = {
+  trackEvent: () => {}, // Empty function, does nothing
+  trackPageview: () => {}, // Similarly, an empty function
+};
+
 const AnalyticsPlugin = {
   install(Vue, options) {
-    const plausible = Plausible(options);
+    // Check for Do Not Track setting
+    const doNotTrackeIsEnabled = navigator.doNotTrack === "1";
+    const analytics = doNotTrackeIsEnabled ? mockAnalytics : Plausible(options);
 
-    Vue.prototype.$analytics = plausible;
+    Vue.prototype.$analytics = analytics;
 
-    if (options.router) {
+    if (options.router && !doNotTrackeIsEnabled) {
       options.router.afterEach((to) => {
-        plausible.trackPageview({ path: to.fullPath });
+        analytics.trackPageview({ path: to.fullPath });
       });
     }
   },
