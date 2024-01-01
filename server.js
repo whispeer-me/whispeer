6891f88e99724eac22df8842a2c0bea586890971
Nesim Tunc <nesimtunc@gmail.com>
2024-01-01 20:15:23 +0900

    Add catch-all route for serving index.html

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require("path");
 const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.get('*', function(_req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 const port = process.env.PORT || 80;
 app.listen(port);
+
+console.log("App started and listening on port: " + port);
