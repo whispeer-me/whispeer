77bc37108ff9a3d95916a51c53606716a41c0494
Nesim Tunc <nesimtunc@gmail.com>
2024-01-02 17:34:09 +0900

    Create an app footer

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,18 @@
     <hr class="divider" />
     <router-view />
     <AnalyticsConsentBanner @consent-given="initAnalytics" />
+    <AppFooter />
   </div>
 </template>
 
 <script>
 import AnalyticsConsentBanner from "@/components/AnalyticsConsentBanner.vue";
+import AppFooter from "@/components/AppFooter.vue";
 
 export default {
   components: {
     AnalyticsConsentBanner,
+    AppFooter,
   },
   methods: {
     initAnalytics() {
@@ -37,6 +40,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
 body {
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,50 @@
+<template>
+  <footer class="app-footer">
+    <AnalyticsConsentToggle @consent-given="handleConsentGiven" />
+    <p class="footer-text">
+      © 2024 Whispeer.me
+      <router-link to="/privacy-policy" class="footer-link"
+        >Privacy Policy</router-link
+      >.
+    </p>
+  </footer>
+</template>
+
+<script>
+import AnalyticsConsentToggle from "@/components/AnalyticsConsentToggle.vue";
+
+export default {
+  components: {
+    AnalyticsConsentToggle,
+  },
+  methods: {
+    handleConsentGiven() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/app.scss";
+
+.app-footer {
+  background-color: rgba(0, 0, 0, 0.7);
+  color: $primary-color;
+  padding: 20px;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+
+  .footer-text {
+    margin-top: 10px;
+  }
+
+  .footer-link {
+    color: $primary-color;
+    &:hover {
+      color: $hover-color;
+    }
+  }
+}
+</style>
