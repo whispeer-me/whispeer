c9e8d71ba91f89db353c0c3011eb7a48dba0b300
Nesim Tunc <nesimtunc@gmail.com>
2024-01-03 15:27:50 +0900

    Lint

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const path = require("path");
 const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
-app.get('*', function(_req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get("*", function (_req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 const port = process.env.PORT || 80;
