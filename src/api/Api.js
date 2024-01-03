961c48faf72c6406d92cd90024bbbcc2b610bbeb
Nesim Tunc <nesimtunc@gmail.com>
2024-01-03 16:56:33 +0900

    Make Api layer generic and debug enabled for dev env

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,23 +1,75 @@
 import axios from "axios";
+import qs from "qs";
 
-const API_URL = process.env.VUE_APP_API_URL || "http://localhost:3000";
-
-const Api = {
-  async get(url) {
-    try {
-      const response = await axios.get(`${API_URL}${url}`);
-      return response.data;
-    } catch (error) {
-      throw new Error("API Error: " + error.message);
-    }
-  },
-  post(url, data) {
-    try {
-      return axios.post(`${API_URL}${url}`, data);
-    } catch (error) {
-      throw new Error("API Error: " + error.message);
-    }
-  },
+const baseURL = process.env.VUE_APP_API_BASE_URL;
+const contentType = "application/json";
+const commonHeaders = {
+  Pragma: "no-cache",
+  Accept: contentType,
+  "Content-Type": contentType,
+  "Cache-Control": "no-cache, no-store, must-revalidate",
 };
 
-export default Api;
+const apiConfig = {
+  baseURL: baseURL,
+  withCredentials: false,
+  timeout: 60000, // 1 minute
+  headers: commonHeaders,
+  paramsSerializer: (params) => qs.stringify(params, { indices: false }),
+};
+
+class Api {
+  constructor() {
+    this.axiosInstance = axios.create(apiConfig);
+
+    if (process.env.NODE_ENV === "development") {
+      this.enableDebugForDevelopment();
+    }
+  }
+
+  getUri() {
+    return this.axiosInstance.getUri(apiConfig);
+  }
+
+  request() {
+    return this.axiosInstance.request(apiConfig);
+  }
+
+  get(url) {
+    return this.axiosInstance.get(url, apiConfig);
+  }
+
+  options(url) {
+    return this.axiosInstance.options(url, apiConfig);
+  }
+
+  head(url) {
+    return this.axiosInstance.head(url, apiConfig);
+  }
+
+  post(url, data) {
+    return this.axiosInstance.post(url, data, apiConfig);
+  }
+
+  success(response) {
+    return response.data;
+  }
+
+  error(error) {
+    throw error;
+  }
+
+  enableDebugForDevelopment() {
+    this.axiosInstance.interceptors.request.use((request) => {
+      console.log("Starting Request", request.url);
+      return request;
+    });
+
+    this.axiosInstance.interceptors.response.use((response) => {
+      console.log("Response:", response.data);
+      return response;
+    });
+  }
+}
+
+export default new Api(apiConfig);
