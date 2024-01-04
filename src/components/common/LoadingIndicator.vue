6aaf9fb18e7e6113fd839ae1bfbe2cdb7f7b5c1e
Nesim Tunc <nesimtunc@gmail.com>
2024-01-04 15:32:04 +0900

    Create a loading indicator component

---
+++ src/components/common/LoadingIndicator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="isLoading" class="loading-indicator">{{ text }}</div>
+</template>
+
+<script>
+export default {
+  name: "LoadingIndicator",
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: String,
+      default: "L o a d i n g . . .",
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/app.scss";
+</style>
