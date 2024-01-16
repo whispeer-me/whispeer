11af2fbd4d552f2dc93296136cc84a698f9329f1
Nesim Tunc <nesimtunc@gmail.com>
2024-01-16 20:36:58 +0900

    Disable x-powered-by header in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const app = express();
 
+app.disable("x-powered-by");
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 app.get("*", function (_req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
