0dcc2a63c78a6004650a8103b0acaca0acee7c0d
Nesim Tunc <nesimtunc@gmail.com>
2024-01-20 17:37:51 +0900

    Improve Error Handling and User Feedback in Crypto and Message Services
    This commit enhances error handling in CryptoService and MessageService,
    improving the user experience by providing more informative error messages.
    Also, refactors CreateMessageView to validate message content and passphrase
    before submission and improves the user feedback mechanism for clipboard operations.
    
    - Wrap encryption and message creation logic in try-catch blocks
    - Implement user-friendly error messages
    - Refactor message validation logic in CreateMessageView
    - Add constants for clipboard message duration
    - Improve clipboard success and failure handling methods

--- src/services/MessageService.js
+++ src/services/MessageService.js
@@ -7,8 +7,15 @@ export default {
   },
 
   async createMessage(message) {
-    const response = await Api.post("/message", message);
-    return response.data.data;
+    try {
+      const response = await Api.post("/message", message);
+      return response.data.data;
+    } catch (error) {
+      throw {
+        message:
+          "Error occured while submitting the message to the server. Please try again.",
+      };
+    }
   },
 
   async getStats() {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,3 +9,5 @@ export const TOOLTIP_META_ENTER_KEY_VIEWS_MAX_COUNT = 10;
 
 export const DISCLAIMER_VIEWS_COUNT = "disclaimerViewsCount";
 export const DISCLAIMER_VIEWS_MAX_COUNT = 10;
+
+export const CLIPBOARD_INFORM_MESSAGE_DURATION = 3000; // 3 seconds
