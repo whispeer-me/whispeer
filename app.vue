8ebbb70dc65e3dd8e1742b7b9828a15f260d8503
Nesim Tunc <nesimtunc@gmail.com>
2024-01-22 08:52:55 +0900

    Move app related css to app's css

--- app.vue
+++ app.vue
@@ -11,18 +11,4 @@
 <style scoped lang="scss">
 @import "@/assets/scss/app.scss";
 @import "@/assets/scss/home.scss";
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  min-height: 100vh;
-}
-
-.divider {
-  border: none;
-  height: 2px;
-  background-color: $divider-color;
-  margin: 0;
-}
 </style>
