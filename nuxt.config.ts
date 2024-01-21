a79b45b0096fc3111a076c22e6277da90b53a5fd
Nesim Tunc <nesimtunc@gmail.com>
2024-01-21 22:33:48 +0900

    Add default meta tags to the config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,44 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: true,
+  app: {
+    head: {
+      title: "Whispeer - Secure, Private Messaging",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Whispeer offers a secure way to create and share encrypted messages that auto-expire in 24 hours",
+        },
+        {
+          name: "keywords",
+          content:
+            "secure messaging, private communication, encrypted messaging, message encryption, auto-expiring messages, data security, privacy, anonymous messaging",
+        },
+      ],
+      link: [
+        { rel: "icon", href: "/favicon.ico" },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon/32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon/16x16.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/favicon/apple-touch-icon.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
+    },
+  },
   css: [
     "~/assets/scss/app.scss",
     "~/assets/scss/header.scss",
