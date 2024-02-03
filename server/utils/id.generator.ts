bb39ad39a3f32980104e6c33bbe89757026cc9a8
Nesim Tunc <nesimtunc@gmail.com>
2024-02-03 11:59:08 +0900

    Add Random ID generator from backend

---
+++ server/utils/id.generator.ts
@@ -0,0 +1,16 @@
+import { randomInt } from "crypto";
+
+export class IDGenerator {
+  static generate(length: number): string {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(randomInt(charactersLength));
+    }
+
+    return result;
+  }
+}
