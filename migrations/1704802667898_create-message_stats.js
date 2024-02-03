5bae2090f38056b0bd5775abd1c81c94392c3d92
Nesim Tunc <nesimtunc@gmail.com>
2024-02-03 11:50:39 +0900

    Add previous migrations from backend repo

---
+++ migrations/1704802667898_create-message_stats.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+exports.up = pgm => {
+  pgm.createTable('message_stats', {
+    id: 'id', // auto-incrementing integer id
+    created_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0
+    },
+    view_count: {
+      type: 'integer',
+      notNull: true,
+      default: 0
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    }
+  });
+
+
+  // Let's insert an initial row
+  pgm.sql('INSERT INTO message_stats (created_count, view_count) VALUES (0, 0)');
+
+};
+
+exports.down = pgm => {
+  pgm.dropTable('message_stats');
+};
