bf5a81cc508560d7c3f04327bc118c670d211734
Nesim Tunc <nesimtunc@gmail.com>
2024-02-06 07:15:35 +0900

    Switching to bun

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-# Run database migrations
-npm run prestart
 
-# Start the application
-exec npm start
+# Run database migrations
+bun run migrate
+
+# Start the application, assuming the build output is correctly pointed by the Dockerfile ENTRYPOINT
+# This might be redundant if the ENTRYPOINT in Dockerfile directly starts the app
+exec bun run start
