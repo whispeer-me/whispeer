1fc050b0fc24e4cd6732645f8197fdcccc9676e8
Nesim Tunc <nesimtunc@gmail.com>
2024-02-11 17:01:54 +0900

    Export Message Stats into a standalone component

---
+++ components/message/Stats.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="statistics">
+    <h2 class="stats-title">Messages</h2>
+    <div class="stat-groups">
+      <div class="stat-item">
+        <span class="stat-value">{{ formatNumber(stats.createdCount) }}</span>
+        <p>Created</p>
+      </div>
+      <div class="stat-item">
+        <span class="stat-value">{{ formatNumber(stats.viewCount) }}</span>
+        <p>Viewed</p>
+      </div>
+      <div class="stat-item">
+        <span class="stat-value">{{ formatNumber(stats.expiringSoonCount) }}</span>
+        <p>Expiring Soon</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, ref } from "vue";
+
+const stats = ref({
+  createdCount: 0,
+  viewCount: 0,
+  expiringSoonCount: 0
+});
+
+async function fetchStats() {
+  try {
+    const { created_count, expiring_soon_count, view_count } = (
+      await messageService.getStats()
+    ).data;
+    stats.value.createdCount = created_count;
+    stats.value.expiringSoonCount = expiring_soon_count;
+    stats.value.viewCount = view_count;
+  } catch (err) {
+    // Handle error
+  }
+}
+
+onMounted(() => {
+  fetchStats();
+  // 30-second interval to fetch data
+  const interval = setInterval(fetchStats, 30000);
+
+  onBeforeUnmount(() => {
+    clearInterval(interval);
+  });
+});
+</script>
+
+<style scoped lang="scss">
+.statistics {
+  text-align: center;
+  margin-top: 80px;
+  color: $primary-color;
+
+  .stats-title {
+    font-size: xx-large;
+    margin-bottom: 40px;
+  }
+
+  .stat-groups {
+    display: flex;
+    justify-content: center;
+
+    @media (max-width: 480px) {
+      width: 80%;
+      margin: 0 auto;
+    }
+
+    .stat-item {
+      margin: 0 20px;
+      text-align: center;
+
+      .stat-value {
+        font-size: $heading-font-size;
+        margin-bottom: 0.5em;
+        transition: transform 0.5s ease-in-out;
+      }
+
+      p {
+        font-size: $text-font-size;
+        color: $secondary-color;
+
+        @media (max-width: 480px) {
+          font-size: 20px;
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,4 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, ref } from "vue";
-
-const createdCount = ref(0);
-const expiringSoonCount = ref(0);
-const viewCount = ref(0);
-const ctaButtonTitle = ref("Create a secure message");
-
-async function fetchData() {
-  try {
-    const { created_count, expiring_soon_count, view_count } = (
-      await messageService.getStats()
-    ).data;
-    createdCount.value = created_count;
-    expiringSoonCount.value = expiring_soon_count;
-    viewCount.value = view_count;
-  } catch (err) {
-    // Handle error
-  }
-}
-
-onMounted(() => {
-  fetchData();
-  // 30-second interval to fetch data
-  const interval = setInterval(fetchData, 30000);
-
-  onBeforeUnmount(() => {
-    clearInterval(interval);
-  });
-});
-
 function redirectToCreateMessagePage() {
   navigateTo({ path: "/m/new", query: { ref: "cta" } });
 }
@@ -43,26 +13,10 @@ function redirectToCreateMessagePage() {
     </p>
 
     <button class="cta-button" @click="redirectToCreateMessagePage">
-      {{ ctaButtonTitle }}
+      Create a secure message
     </button>
 
-    <div class="statistics">
-      <h2 class="stats-title">Messages</h2>
-      <div class="stat-groups">
-        <div class="stat-item">
-          <span class="stat-value">{{ formatNumber(createdCount) }}</span>
-          <p>Created</p>
-        </div>
-        <div class="stat-item">
-          <span class="stat-value">{{ formatNumber(viewCount) }}</span>
-          <p>Viewed</p>
-        </div>
-        <div class="stat-item">
-          <span class="stat-value">{{ formatNumber(expiringSoonCount) }}</span>
-          <p>Expiring Soon</p>
-        </div>
-      </div>
-    </div>
+    <MessageStats />
   </div>
 </template>
 
