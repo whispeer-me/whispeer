5cae132d93476eb26d34ddea88104c8e8bbfa038
Nesim Tunc <nesimtunc@gmail.com>
2024-02-12 01:35:10 +0900

    Create a common image component

---
+++ components/common/Image.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtImg preload :src="src" :alt="alt" class="image" fit="cover" format="webp" loading="lazy" />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  src: String,
+  alt: String
+})
+</script>
+
+<style scoped lang="scss">
+.image {
+  width: 90vw;
+  height: 56.25vw;
+  max-height: 100vh;
+  object-fit: cover;
+}
+</style>
