269cc28b956956ca90436865b0b7d4022708d06b
Nesim Tunc <nesimtunc@gmail.com>
2024-02-12 11:17:34 +0900

    Refactor HIW page

---
+++ components/how/Section.vue
@@ -0,0 +1,46 @@
+<template>
+<section class="section">
+  <h2>{{ title }}</h2>
+  <div v-if="image" class="image-container">
+    <CommonImage :src="image" :alt="title" />
+  </div>
+  <p>
+   {{ description }}
+  </p>
+  <ul v-if="items">
+    <li v-for="(item, index) in items" :key="index"> {{ item }} </li>
+  </ul>
+</section>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    title: String,
+    description: String,
+    image: {
+      type: String,
+      required: false,
+    },
+    items: {
+      type: Array as PropType<string[]>,
+      required: false,
+      default: () => [],
+    }
+  })
+</script>
+
+<style scoped lang="scss">
+.section {
+  h2 {
+    font-family: $secondary-font;
+  }
+
+  p {
+    font-size: large;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+}
+</style>
--- pages/how-it-works.vue
+++ pages/how-it-works.vue
@@ -10,53 +10,27 @@
 
     <CommonImage src="/images/whispeer-how-it-works.webp" alt="Whispeer Security Features" />
 
-    <section class="section client-side-encryption">
-      <h2>Your Messages, Encrypted Before They Leave Your Device</h2>
-      <div class="encryption-diagram"> </div>
-      <p>
-        When you create a secure message on Whispeer, it's encrypted right
-        within your web browser or mobile device. This means that only you and
-        the person you share the passphrase with can ever read the message – not
-        even Whispeer can access it.
-      </p>
-    </section>
+    <HowSection title="Your Messages, Encrypted Before They Leave Your Device"
+      description="When you create a secure message on Whispeer, it's encrypted right within your web browser or mobile device. This means that only you and the person you share the passphrase with can ever read the message – not even Whispeer can access it."
+      image="/images/whispeer-how-your-messages.webp" />
 
-    <section class="section secure-message-handling">
-      <h2>Disappearing Messages: How We Do It</h2>
-      <div class="disappearing-message-diagram"> </div>
-      <p>
-        Every message on Whispeer has a lifespan of 24 hours. After that, it's
-        automatically and permanently deleted from our servers. You don't have to
-        worry about old messages hanging around.
-      </p>
-    </section>
+    <HowSection title="Your Messages, Encrypted Before They Leave Your Device"
+      description="When you create a secure message on Whispeer, it's encrypted right within your web browser or mobile device. This means that only you and the person you share the passphrase with can ever read the message – not even Whispeer can access it."
+      image="/images/whispeer-how-your-messages.webp" />
 
-    <section class="section privacy-first-analytics">
-      <h2>Understanding Usage, Protecting Your Data</h2>
-      <div class="privacy-analytics-diagram"> </div>
-      <p>
-        To make Whispeer better, we use anonymized analytics. This means we
-        track overall usage patterns, but without collecting any personal
-        information about you. We'll never compromise your privacy for data.
-      </p>
-    </section>
+    <HowSection title="Disappearing Messages: How We Do It"
+      description="Every message on Whispeer has a lifespan of 24 hours. After that, it's automatically and permanently deleted from our servers. You don't have to worry about old messages hanging around."
+      image="/images/whispeer-disappearing-messages.webp" />
 
-    <section class="section user-responsibilities">
-      <h2>Strong Passphrases are Key</h2>
-      <div class="strong-passphrases-diagram"> </div>
-      <p>
-        When you choose to encrypt a message, your passphrase is vital. Here's
-        how to make strong passphrases:
-      </p>
-      <ul>
-        <li>Use a mix of letters, numbers, and symbols.</li>
-        <li>Make them at least 12 characters long.</li>
-        <li>Never reuse passphrases on other websites or services.</li>
-      </ul>
-    </section>
+    <HowSection title="Understanding Usage, Protecting Your Data"
+      description="To make Whispeer better, we use anonymized analytics. This means we track overall usage patterns, but without collecting any personal information about you. We'll never compromise your privacy for data."
+      image="/images/whispeer-disappearing-messages.webp" />
+
+    <HowSection title="Strong Passphrases are Key"
+      description="When you choose to encrypt a message, your passphrase is vital. Here's how to make strong passphrases:"
+      :items="['Use a mix of letters, numbers, and symbols.', 'Make them at least 12 characters long.', 'Never reuse passphrases on other websites or services.']" />
   </section>
 </template>
-
 
 <script setup lang="ts">
 useHead({
@@ -81,22 +55,18 @@ useHead({
     font-family: $secondary-font;
   }
 
-  h2 {
-    font-family: $secondary-font;
-  }
-
   p {
     color: $secondary-color;
     font-family: $secondary-font;
-  }
-
-  ul {
-    list-style-type: none;
   }
 }
 
 .how-it-works-page {
   margin: 0px 20px 0px 20px;
+
+  p {
+    font-size: large;
+  }
 }
 
 .section {
