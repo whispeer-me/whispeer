17892f9276aa7f6125722428b0ca5fe0e9248a8c
Nesim Tunc <nesimtunc@gmail.com>
2024-02-15 01:25:13 +0900

    Style the inputs and messages

--- pages/c/index.vue
+++ pages/c/index.vue
@@ -1,62 +1,100 @@
 <template>
-  <div id="messages">
-    <ul>
-      <li v-for="message in messages" :key="message.id">
-        <div class="item">
-          {{ message.content }}
-        </div>
-      </li>
-    </ul>
+  <div class="chat-container">
+    <div id="messages">
+      <ul>
+        <li v-for="message in messages" :key="message.id" :class="getClass(message.id)">
+          <div class="item">
+            {{ message.content }}
+          </div>
+        </li>
+      </ul>
+    </div>
+
   </div>
 
-  <div>
+  <div class="message-input">
     <input type="text" v-model="message" placeholder="Type your message here ..." @keyup.enter="sendMessage" />
-
-    <button type="submit" @click="sendMessage">
-      Send
-    </button>
+    <button type="submit" @click="sendMessage">Send</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
+
+let id = 1;
 
 const message = ref("")
 const messages = ref([{
-  id: 0,
+  id: id++,
   content: "Hello, World!"
 }]);
-let id = 1;
+
 const sendMessage = async () => {
   messages.value.push({
-    id,
+    id: id++,
     content: message.value
   });
 
-  id++;
-
   message.value = "";
+}
+
+const getClass = (id: number) => {
+  return id % 2 == 0 ? "right" : "left"
 }
 
 </script>
 
 <style scoped lang="scss">
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 80vh;
+}
+
 #messages {
+  margin-bottom: 20px;
+  flex-grow: 1;
+
   ul {
     list-style-type: none;
+    padding: 0;
 
     li {
       color: $secondary-color;
       font-family: $secondary-font;
+      /* top | right | bottom | left */
+      margin: 10px 15px 10px 15px;
       text-align: right;
-      margin-top: 12px;
 
       .item {
+        display: inline-block;
+        max-width: 80%;
         border: 1px solid #fff;
         border-radius: 30px;
         padding: 15px;
+        background-color: $background-color;
+        word-wrap: break-word;
       }
     }
+  }
+}
+
+.message-input {
+  display: flex;
+  gap: 10px;
+  margin: 0 20px;
+
+  input {
+    flex-grow: 1;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+
+  button {
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
   }
 }
 </style>
