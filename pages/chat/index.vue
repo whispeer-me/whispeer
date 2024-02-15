9dc2fcaa9c3a758a738bc69bcefa1e64b68ad047
Nesim Tunc <nesimtunc@gmail.com>
2024-02-15 17:23:24 +0900

    Scroll to bottom if user hasn't scrolled up

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chat-container">
-    <div class="messages-container">
+    <div class="messages-container" ref="messagesContainer" @scroll="checkScrollPosition">
       <div id="messages">
         <ul>
           <li v-for="message in messages" :key="message.id" :class="getClass(message.id)">
@@ -12,7 +12,9 @@
       </div>
     </div>
   </div>
-
+  <div v-if="showNewMessageNotification" class="new-message-notification" @click="scrollToBottom">
+    🔔 New message 📨
+  </div>
   <div class="message-input">
     <input type="text" v-model="message" placeholder="Type your message here ..." @keyup.enter="sendMessage" />
     <button type="submit" @click="sendMessage">Send</button>
@@ -20,7 +22,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref, watch, nextTick } from "vue";
 
 let id = 1;
 
@@ -30,6 +32,10 @@ const messages = ref([{
   content: "Hello, World!"
 }]);
 
+const messagesContainer = ref(null);
+const userHasScrolledUp = ref(false);
+const showNewMessageNotification = ref(false);
+
 const sendMessage = async () => {
   messages.value.push({
     id: id++,
@@ -37,11 +43,45 @@ const sendMessage = async () => {
   });
 
   message.value = "";
+  maybeScrollToBottom();
 }
 
 const getClass = (id: number) => {
   return id % 2 == 0 ? "sender" : "receiver"
 }
+
+const checkScrollPosition = () => {
+  // How far from the bottom should consider user wants to see old message
+  // So that we won't scroll to bottom on new message arrives
+  // But we want to notify them on new message arrives in such case
+  const threshold = 100;
+  const position = messagesContainer.value.scrollHeight - messagesContainer.value.scrollTop - messagesContainer.value.clientHeight;
+  userHasScrolledUp.value = position > threshold;
+  // Shall notify the user?
+  if (!userHasScrolledUp.value) {
+    showNewMessageNotification.value = userHasScrolledUp.value;
+  }
+}
+
+const maybeScrollToBottom = async () => {
+  await nextTick();
+  if (!userHasScrolledUp.value) {
+    messagesContainer.value.scrollTop = messagesContainer.value.scrollHeight;
+    showNewMessageNotification.value = false;
+  } else {
+    showNewMessageNotification.value = true;
+  }
+};
+
+const scrollToBottom = async () => {
+  await nextTick();
+  messagesContainer.value.scrollTo({
+    top: messagesContainer.value.scrollHeight,
+    behavior: "smooth"
+  });
+  showNewMessageNotification.value = false;
+}
+
 
 </script>
 
@@ -106,5 +146,16 @@ const getClass = (id: number) => {
     border: none;
     cursor: pointer;
   }
+}
+
+.new-message-notification {
+  color: #fff;
+  font-family: $secondary-font;
+  border: 1px solid #fff;
+  border-radius: 100px;
+  margin: 20px auto;
+  padding: 10px;
+  width: 200px;
+  cursor: pointer;
 }
 </style>
