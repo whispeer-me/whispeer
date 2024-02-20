ab23e97b88df7811e0f02ae10e7dbb168135ebb2
Nesim Tunc <nesimtunc@gmail.com>
2024-02-20 19:38:08 +0900

    Create wrapper around Plausible Analytics module

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,45 +1,16 @@
 import { vi } from "vitest";
-import type { PlausibleOptions, EventOptions } from "plausible-tracker";
-
-// Mocking Nuxt's useNuxtApp to include $plausible
-// with detailed mock implementations
-vi.mock("#app", () => ({
-  useNuxtApp: vi.fn(() => ({
-    config: {},
-    defineNuxtConfig: vi.fn(() => {}),
-    $plausible: {
-      isEnabled: false,
-      trackLocalhost: false,
-      autoPageviews: false,
-      autoOutboundTracking: true,
-      trackPageview: vi.fn(
-        (_eventData?: PlausibleOptions, _options?: EventOptions) => {}
-      ),
-      trackEvent: vi.fn(
-        (
-          _eventName: string,
-          _options?: EventOptions,
-          _eventData?: PlausibleOptions
-        ) => {}
-      ),
-    },
-  })),
-}));
-
-// Mocking Plausible functionality with parameters
-vi.mock("@nuxtjs/plausible", () => ({
-  useTrackPageview: vi.fn(
-    (eventData?: PlausibleOptions, options?: EventOptions) => {}
-  ),
-  useTrackEvent: vi.fn(
-    (
-      _eventName: string,
-      _options?: EventOptions,
-      _eventData?: PlausibleOptions
-    ) => {}
-  ),
-}));
 
 // Mock for useHead
 vi.mock("@unhead/vue");
 vi.mocked(useHead).mockImplementation(vi.fn());
+
+// Mock for Analytics
+vi.mock("~/composables/useAnalytics.ts", async (importOriginal) => {
+  const actual = await importOriginal();
+  return Object.assign({}, actual, {
+    default: () => ({
+      trackEvent: vi.fn(),
+      trackPageView: vi.fn(),
+    }),
+  });
+});
