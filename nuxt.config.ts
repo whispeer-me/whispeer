d0e52031f3a33560d27705442f283baa7f7cc398
Nesim Tunc <nesimtunc@gmail.com>
2024-02-24 15:32:40 +0900

    Remove prerender config as it hang the build process

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,11 +14,6 @@ export default defineNuxtConfig({
     autoOutboundTracking: true,
   },
   ssr: true,
-  nitro: {
-    prerender: {
-      routes: ["/", "/code", "/contact", "/privacy-policy", "/the-story"],
-    },
-  },
   app: {
     head: {
       htmlAttrs: {
