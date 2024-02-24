7662aacea370702003f7009c820bb92b5174c980
Nesim Tunc <nesimtunc@gmail.com>
2024-02-24 14:33:50 +0900

    Refactor NavBar
    Create a new MenuItem component to reduce duplication

---
+++ components/layout/MenuItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  isMobile: boolean;
+  title: string;
+  icon: string;
+  to: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink :to="to" class="nav-link">
+    <NuxtPicture
+      preload
+      v-if="isMobile"
+      :src="`/icons/menu/${icon}`"
+      :alt="title"
+      class="icon-mobile svg-color"
+      width="24"
+      height="24"
+      loading="lazy"
+    />
+    <span v-if="!isMobile">{{ title }}</span>
+  </NuxtLink>
+</template>
